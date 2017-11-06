8791754b5a4caa997ce6bb5d05e75b603ecf72a4
myvary <727484328@qq.com>
2017-11-06 20:54:42 +0800

    安装expect

--- install_squid.sh
+++ install_squid.sh
@@ -5,6 +5,7 @@ echo Y | sudo apt-get install curl
 echo Y | sudo apt-get install squid3
 echo Y | sudo apt-get install apache2-utils
 cd /etc/squid3
+sudo apt-get install expect
 sudo rm -f squid3.conf
 curl -O 'https://raw.githubusercontent.com/myvary/squid3/master/squid.conf'
 sudo htpasswd  -c  /etc/squid3/passwd  mc_proxy
