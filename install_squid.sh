bae6a15d43e4bbd13fc8330b5b7c9a26729e7bc7
myvary <727484328@qq.com>
2017-12-04 09:48:27 +0800

    修改配置

--- install_squid.sh
+++ install_squid.sh
@@ -4,9 +4,9 @@ cd ~
 echo Y | sudo apt-get install curl
 echo Y | sudo apt-get install squid3
 echo Y | sudo apt-get install apache2-utils
-cd /etc/squid3
-sudo rm -f squid3.conf
+cd /etc/squid
+sudo rm -f squid.conf
 sudo curl -O 'https://raw.githubusercontent.com/myvary/squid3/master/squid.conf'
-sudo htpasswd  -c  /etc/squid3/passwd  mc_proxy
-sudo squid3 -k reconfigur
-sudo service squid3 restart
+sudo htpasswd  -c  /etc/squid/passwd  mc_proxy
+sudo squid -k reconfigur
+sudo service squid restart
